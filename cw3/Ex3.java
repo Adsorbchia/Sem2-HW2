4915774fc0ba125ee9cfe43934dfe828368b27de
Natalya <117934761+Adsorbchia@users.noreply.github.com>
2023-03-11 00:22:14 +0400

    feat:add new solution task#3

---
+++ cw3/Ex3.java
@@ -0,0 +1,31 @@
+package org.example.seminar2.cw3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ex3 {
+    /**
+     * Дан массив целых чисел. Найти количество пар соседних элементов, где первый элемент вдвое больше второго
+      */
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int []array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i]=sc.nextInt();
+
+        }
+        System.out.println(Arrays.toString(array));
+        int res=0;
+        for (int i = 0; i < size-2; i++) {
+            if (array[i]/array[i+1]==2){
+                res+=1;
+
+            }
+
+        }
+        System.out.println(res);
+        sc.close();
+    }
+}
