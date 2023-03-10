6294d6a5cbad660d5de0d9a9058793a1bb5ada91
Natalya <117934761+Adsorbchia@users.noreply.github.com>
2023-03-10 23:57:40 +0400

    feat:add new solution task#1

--- cw1/Ex1.java
+++ cw1/Ex1.java
@@ -1,4 +1,20 @@
 package org.example.seminar2.cw1;
 
+import java.util.Scanner;
+
 public class Ex1 {
+// Дана последовательность из N целых чисел. Найти сумму чисел, оканчивающихся на 5, перед которыми идет четное число.
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int a= sc.nextInt();
+    int res =0;
+    for (int i = 0; i < n-1; i++) {
+        int b = sc.nextInt();
+        if (a%10==5 && b%2==0){
+            res+=a;
+        }
+        a=b;
+    }System.out.println(res);
+}
 }
