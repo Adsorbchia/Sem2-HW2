571eaa6f1d74721f5f39c2ea4db11945d7340959
Natalya <117934761+Adsorbchia@users.noreply.github.com>
2023-03-12 11:41:21 +0400

    feat: add new solution to task

--- cw4/Ex4.java
+++ cw4/Ex4.java
@@ -8,8 +8,12 @@ public class Ex4 {
         Vector vector2 = new Vector(1.0,2.0,1.0);
         System.out.println(vector1);
         System.out.println(vector2);
-        System.out.println("длинна вектора = " +vector1.LengthVector());
-        System.out.println(" скалярное произведение = " +vector1.scMulti(vector2));
+        System.out.println("длинна вектора = " + vector1.LengthVector());
+        System.out.println("скалярное произведение = " + vector1.scMulti(vector2));
+        System.out.println("векторное произведение = " + vector1.vectorMulti(vector2));
+        System.out.println("Косинус угла = " + vector1.cosVector(vector2));
+        System.out.println("Сумму векторов = " + vector1.sumVector(vector2));
+        System.out.println("Разность векторов = " + vector1.differenceVector(vector2));
 
     }
 
